c3309fa28f8a629f3354377e99d491fd9891ade4
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2026-02-21 00:44:21 +0200

    First commit

---
+++ ame-ypd-backend/src/main/java/com/ame_ypd_backend/AmeM4YpdWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ame_ypd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeM4YpdWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmeM4YpdWebsiteApplication.class, args);
+	}
+
+}
---
+++ ame-ypd-backend/src/test/java/com/ame_ypd_backend/AmeM4YpdWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ame_ypd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeM4YpdWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
