085d053fdc9c5682bf45b8929d773fbaa98461b9
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2026-02-27 01:36:31 +0200

    fix: removed unused import

--- ame-ypd-backend/src/main/java/com/ame_ypd_backend/controller/MediaController.java
+++ ame-ypd-backend/src/main/java/com/ame_ypd_backend/controller/MediaController.java
@@ -9,7 +9,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
