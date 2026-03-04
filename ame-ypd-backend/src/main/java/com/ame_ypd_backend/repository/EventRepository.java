cf9a4c1cc4ba1f4c268e409d6e6f5530aeb8ee82
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2026-03-05 01:30:38 +0200

    Fix: prevented race condition by add locks

--- ame-ypd-backend/src/main/java/com/ame_ypd_backend/repository/EventRepository.java
+++ ame-ypd-backend/src/main/java/com/ame_ypd_backend/repository/EventRepository.java
@@ -5,9 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.persistence.LockModeType;
+
+
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT e FROM Event e WHERE e.eventId = :eventId")
+    Optional<Event> findByIdWithLock(Long eventId);
 
     // Spring Data JPA auto-generates SQL from these method names!
     // This is O(log n) indexed query — your algorithmic awareness matters here
