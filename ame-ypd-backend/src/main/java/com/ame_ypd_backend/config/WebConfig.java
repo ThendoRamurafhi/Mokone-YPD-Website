f7661eba50c166f47b7875989eaf33375470f763
Thendo Ramurafhi <thendoramurafhi@gmail.com>
2026-05-06 10:57:35 +0200

    feat: add file

---
+++ ame-ypd-backend/src/main/java/com/ame_ypd_backend/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.ame_ypd_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+          // Resolve absolute path so it works regardless of working directory
+        Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
+        String resourceLocation = "file:" + uploadPath.toString() + "/";
+
+        // Serve uploaded media files
+        registry.addResourceHandler("/api/v1/media/files/**")
+                .addResourceLocations(resourceLocation);
+                //.setCachePeriod(3600);   // cache 1 hour in browser
+
+        // Also serve without the /api/v1 prefix for direct access
+        registry.addResourceHandler("/api/v1/media/files/**")
+                .addResourceLocations(resourceLocation)
+                .setCachePeriod(3600);
+    }
+}
